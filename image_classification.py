89b4095241a970814bcc0955571301a28d5ada75
hamitov renat <hamitov@live.ru>
2022-11-02 16:30:09 +0300

    modified:   image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -49,7 +49,7 @@ def print_predictions(preds):
         #     st.write(str(tt['translation_text']))
         en_text=str(cl[1]).replace('_'," ")
         trans_ta = translate({"inputs": [">>rus<< "+en_text, ">>tat<< "+en_text,],}, API_URL_ta),
-        for tt in trans_ta:
+        for tt in tuple(trans_ta):
             st.write(str(tt['translation_text']))
 
 
