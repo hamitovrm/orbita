b64c9c192b2c371e13763240901285a2c322c782
hamitovrm <96112608+hamitovrm@users.noreply.github.com>
2022-11-02 19:47:08 +0300

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -50,11 +50,12 @@ def print_predictions(preds):
         en_text=str(cl[1]).replace('_'," ")
         trans_ta = translate({"inputs": [">>rus<< "+en_text, ">>tat<< "+en_text,],}, API_URL_ta)
         tr_test=tuple(trans_ta())
-        "Русский:       "+tr_test[0]['translation_text']
-        "Татарский:     "+tr_test[1]['translation_text']
+	for tt in tr_test:
+            str(tt['translation_text'])
+        #"Русский:       "+tr_test[0]['translation_text']
+        #"Татарский:     "+tr_test[1]['translation_text']
 	#trans_ta
-        for tt in tr_test:
-             str(tt['translation_text'])
+       
 
 
 model = load_model()
