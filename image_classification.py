ca2d54909c6bb0e26075f4151ba784d288704ded
hamitovrm <96112608+hamitovrm@users.noreply.github.com>
2022-11-08 16:10:30 +0300

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -49,7 +49,6 @@ def print_predictions(preds):
         #     st.write(str(tt['translation_text']))
         en_text=str(cl[1]).replace('_'," ")
         trans_ta = translate({"inputs": [">>rus<< "+en_text, ">>tat<< "+en_text, ">>deu<< "+en_text,],}, API_URL_ta)
-        st.write(trans_ta)
         tr_test=tuple(trans_ta())
         st.write(str(tr_test))
         for tt in tr_test:
